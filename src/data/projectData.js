4cca42b2482023c19c5f9b1d285113617e5a9b36
jsanchezpla <jsanchezpla@gmail.com>
2026-02-02 16:44:41 +0100

    mobile desing and projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,55 +1,114 @@
 import talentia from "../assets/img-projects/talentia.png";
 import retorika from "../assets/img-projects/retorika.png";
+import lacuriosidaddelviajero from "../assets/img-projects/lacuriosidaddelviajero.png";
+import v30energy from "../assets/img-projects/v30energy.png";
+import andreadavila from "../assets/img-projects/andreadavila.png";
+import centroaumenta from "../assets/img-projects/centroaumenta.png";
+import colorincolorado from "../assets/img-projects/colorincolorado.png";
+import kaleido from "../assets/img-projects/kaleido.png";
+import lauraubeda from "../assets/img-projects/lauraubeda.png";
+import sanchezlazaro from "../assets/img-projects/sanchezlazaro.png";
+
 
 export const proyectos = [
   {
-    titulo: "App web Futuros Talentos",
+    titulo: "Futuros Talentos",
     descripcion:
-      "App de registro de actividades e información de las mismas con autentificacion de usuarios, token de autentificación, PDFs personalizados, hojas de excel personalizadas y firmas de formularios.",
+      "Rediseño integral de la plataforma 'Futuros Talentos' potenciado por Elementor para ofrecer una estética vanguardista y profesional. El proyecto se centró en la modernización de la identidad visual y la implementación de una interfaz altamente intuitiva, optimizando tanto el impacto visual como la experiencia de usuario mediante soluciones técnicas modernas. App de registro de actividades e información de las mismas con autentificacion de usuarios, token de autentificación, PDFs personalizados, hojas de excel personalizadas y firmas de formularios. Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
     imagen: talentia.src,
-    tecnologias: ["React Native", "PostgreeSQL", "Express", "Node"],
+    tecnologias: ["React Native", "React", "PostgreeSQL", "Express", "Node", "Elementor", "Wordpress"],
     demo: "https://front-react-todo.onrender.com",
     codigo: "https://github.com/Lautaro-R-collins/Front-react-todo",
     categoria: "fullstack",
   },
   {
-    titulo: "ERP de Talentia",
+    titulo: "Asesoría Retorika",
+    descripcion:
+      "Este proyecto proporciona mediante scripts la personalización del pluggin de TutorLMS, se añadieron funcionalidades de pago y otras que no tenía el pluggin de forma gratuita. Además de un login y registro personalizados y todo conectado con su programa de gestión(ERP). Aplicación web de gestión de las nuevas utilidades del sitio web de Retorika. Se pueden añadir los diferentes empleados de una empresa que haya contratado un paquete con una serie de cursos, se pueden mirar los resultados de las encuestas y los examenes de los cursos.",
+    imagen: retorika.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP", "React", "Node", "Express", "PostgreeSQL"],
+    demo: "https://c21-05-ft-node-react-mirror-frontend-64qbwssi2-muyvisual.vercel.app/",
+    codigo: "https://github.com/No-Country-simulation/c21-05-ft-node-react",
+    categoria: "fullstack",
+  },
+  {
+    titulo: "La Curiosidad del Viajero",
     descripcion:
       "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
-    imagen: talentia.src,
-    tecnologias: ["React", "PostgreeSQL", "Express", "Node"],
+    imagen: lacuriosidaddelviajero.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "V30 Energy",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: v30energy.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "Espacio Kaleido",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: kaleido.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "Centro aumenta",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: centroaumenta.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "Colorín Colorado",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: colorincolorado.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "Andrea Dávila Nutrición",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: andreadavila.src,
+    tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
+    categoria: "frontend",
+  },
+  {
+    titulo: "Pescados y Marisco Sánchez Lázaro",
+    descripcion:
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: sanchezlazaro.src,
+    tecnologias: ["Javascript", "Electron", "Excel"],
     demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
     codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
     categoria: "fullstack",
   },
   {
-    titulo: "Página web Futuros Talentos",
+    titulo: "Laura Úbeda",
     descripcion:
-      "Rediseño integral de la plataforma 'Futuros Talentos' potenciado por Elementor para ofrecer una estética vanguardista y profesional. El proyecto se centró en la modernización de la identidad visual y la implementación de una interfaz altamente intuitiva, optimizando tanto el impacto visual como la experiencia de usuario mediante soluciones técnicas modernas.",
-    imagen: talentia.src,
-    tecnologias: ["Elementor", "Wordpress"],
-    demo: "https://traker-gastos-ingresos.onrender.com/",
-    codigo: "https://github.com/Lautaro-R-collins/Traker-Gastos-Ingresos",
-    categoria: "frontend",
-  },
-  {
-    titulo: "Asesoría Retorika",
-    descripcion:
-      "Este proyecto proporciona mediante scripts la personalización del pluggin de TutorLMS, se añadieron funcionalidades de pago y otras que no tenía el pluggin de forma gratuita. Además de un login y registro personalizados y todo conectado con su programa de gestión(ERP).",
-    imagen: retorika.src,
+      "Aplicación web de gestión de la app de Futuros Talentos y finanzas de Talentia. Se trata de un CRUD a la base de datos muy personalizado con generación de excels, descarga de PDFs, resolución de mensajes de incidencias y descarga de fotos de tickets de compra.",
+    imagen: lauraubeda.src,
     tecnologias: ["Wordpress", "Elementor", "Javascript", "PHP"],
-    demo: "https://c21-05-ft-node-react-mirror-frontend-64qbwssi2-muyvisual.vercel.app/",
-    codigo: "https://github.com/No-Country-simulation/c21-05-ft-node-react",
+    demo: "https://proyecto-final-rodriguez-sable.vercel.app/",
+    codigo: "https://github.com/Lautaro-R-collins/ProyectoFinal-Rodriguez.git",
     categoria: "frontend",
-  },
-  {
-    titulo: "ERP de Retorika",
-    descripcion:
-      "Aplicación web de gestión de las nuevas utilidades del sitio web de Retorika. Se pueden añadir los diferentes empleados de una empresa que haya contratado un paquete con una serie de cursos, se pueden mirar los resultados de las encuestas y los examenes de los cursos.",
-    imagen: retorika.src,
-    tecnologias: ["React", "Node", "Express", "PostgreeSQL"],
-    demo: "https://traker-gastos-ingresos.onrender.com/",
-    codigo: "https://github.com/Lautaro-R-collins/Traker-Gastos-Ingresos",
-    categoria: "fullstack",
   },
 ];
